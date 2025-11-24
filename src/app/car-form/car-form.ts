83a70442edcd85e6f98d92dd6e468986119fe876
sarvarmarimboyev <sarvar.marimboyev@mail.ru>
2025-11-24 17:50:35 +0500

    make change

--- src/app/car-form/car-form.ts
+++ src/app/car-form/car-form.ts
@@ -1,18 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 import { CarService } from '../services/car.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-car-form',
   imports: [ReactiveFormsModule],
   templateUrl: './car-form.html',
-  styleUrls: ['./car-form.css'],  // <-- FIXED
+  styleUrls: ['./car-form.css'],
   standalone: true,
 })
 export class CarForm {
-
-  // FIXED: inject must be imported
   carService = inject(CarService);
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
 
   reservationForm: FormGroup = new FormGroup({
     checkIn: new FormControl('', [Validators.required]),
@@ -23,13 +24,40 @@ export class CarForm {
     carNumber: new FormControl('', [Validators.required]),
   });
 
+  constructor() {
+    const reservationID = this.activatedRoute.snapshot.paramMap.get('id');
+    if (reservationID) {
+      const reservation = this.carService.getReservationById(+reservationID);
+
+      if (reservation) {
+        this.reservationForm.patchValue({
+          checkInDate: reservation.checkInDate,
+          checkOutDate:reservation.checkOutDate,
+          clientName:reservation.clientName,
+          clientEmail:reservation.clientEmail,
+          carModel:reservation.carModel,
+          carNumber:reservation.carNumber
+        });
+      }
+    }
+  }
+
   onSubmit() {
-    if (this.reservationForm.invalid) return;
-
-
     
     const data = { ...this.reservationForm.value, id: Date.now() };
+    
+    const reservationID = this.activatedRoute.snapshot.paramMap.get('id');
+    
+    if (reservationID) {
+      const reservation = this.carService.getReservationById(+reservationID);
+      if(reservation){
+        this.carService.updatedReservation(reservation)
+ }
+          }
+        }
+  
     this.carService.addReservaion(data);
     this.reservationForm.reset();
+    this.router.navigate(['/list']);
   }
 }
