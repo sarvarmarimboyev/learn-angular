225e788fb780556535e2ede3e6d09f63c0370ec1
sarvarmarimboyev <sarvar.marimboyev@mail.ru>
2025-12-09 17:55:05 +0500

    new project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductList } from './product-list/product-list';
 
 export const routes: Routes = [
 
+
+{path:'',redirectTo:'/products',pathMatch:'full'},
+{path:'products',component:ProductList}, 
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,41 +1,12 @@
-import { Component, effect, signal } from '@angular/core';
-import { UserComponent } from './user/user';
-import { IUser } from './interfaces';
-import { log } from 'console';
-import { Article } from './article/article';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
+  styleUrls: ['./app.css'],
   templateUrl: './app.html',
-  imports: [Article],
-  template:`<section>
-    <app-comments >
-      @defer(on viewport){}
-    </app-comments>
-
-    @placeholder{
-      <p>This is comment place</p>
-    }
-    @loading{
-      <p>This is loading </p>
-    }
-  </section>`
 })
 export class App {
-  email = signal('');
-  emailError = signal('');
 
-  constructor() {
-    effect(() => {
-      const keyword = this.email();
-      if (keyword.length) {
-        console.log(`Searching for: ${keyword}`);
-      }
-    });
-  }
-
-  onEmailChange(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.email.set(input.value);
-  }
 }
